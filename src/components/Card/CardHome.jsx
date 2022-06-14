0f2d1d8031871cef5a3fd3794d8c09afa1a56839
AnasMA94 <anas@labelideas.co>
2022-06-14 17:46:33 +0700

    Update Apps

--- src/components/Card/CardHome.jsx
+++ src/components/Card/CardHome.jsx
@@ -10,7 +10,7 @@ const CardHome = ({ dataUsers }) => {
   let uniqueData = [...new Map(dataUsers.map((item) => [item["id"], item])).values()];
 
   return (
-    <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 sm:gap-x-3 w-full">
+    <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 sm:gap-x-3 w-full">
       {uniqueData?.map((user) => (
         <div key={user.id} className="cursor-pointer w-full md:max-w-xs my-3 bg-white rounded-lg shadow-lg dark:bg-gray-800" onClick={() => toDetail(user.id)}>
           <img className="object-cover rounded-t-md w-full h-56" src={user.avatar} alt="avatar" />
